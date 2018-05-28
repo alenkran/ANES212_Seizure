2d3ba1521a0d292df06969712660a44f34116096
alenkran <choe.christian@gmail.com>
2018-05-28 15:50:12 -0700

    Organized BCH + LMS
    -Now the .edf files are organized and labeled.
    -Started implementing LMS filter

---
+++ scripts/LMS.py
@@ -0,0 +1,293 @@
+################################################################################
+# Authors: Min Cheol Kim, Christian Choe
+
+# Description: Classes for linear filters
+
+# Linear filters implemented so far:
+#   - mu_LMS
+#   - alpha_LMS
+################################################################################
+
+import numpy as np
+
+# This class implements the good old LMS filter
+# Requires the training_set to be a dictionary.
+class mu_LMS_filter:
+    
+    def __init__(self, training_set, mu_fraction=0.5, num_weight=5, bias=False, causal=True, delay=0):
+        self.training_set = training_set
+        self.num_weight = num_weight
+        self.mu_fraction = mu_fraction
+        self.trace_R = self.est_trace_R()
+        self.mu = self.mu_fraction / self.trace_R
+        self.bias = bias
+        self.causal = causal
+        self.delay = delay
+        if bias:
+            self.weight = np.zeros((self.num_weight+1))
+        else:
+            self.weight = np.zeros((self.num_weight))
+    
+    def reset_weight(self):
+        self.weight = np.zeros(len(self.weight))
+
+    def train_single_patient(self, patient_id, stop_half = False, train_cycles=5):
+        W = self.weight # initalize weights
+        weight_path = []
+        error_path = []
+        value = self.training_set[patient_id]
+        cgm = value[:, 1]
+        bgm = value[:, 2]
+        
+        # set start and end index for training, taking into account delay and causality
+        start_idx = np.max([self.num_weight-1, self.delay])
+        if self.causal:
+            end_idx = len(cgm)-1
+            start_idx = np.max([self.num_weight-1, self.delay])
+        else:
+            end_idx = len(cgm)-1-(self.num_weight/2)
+            start_idx = np.max([self.num_weight/2, self.delay])
+        predicted = np.zeros(len(cgm))
+        predicted.fill(np.nan)
+        #end_idx = len(cgm)-1 if self.causal else len(cgm)-1-(self.num_weight/2)
+        for cycle in range(train_cycles):
+            for i in range(start_idx, (end_idx - start_idx)/2):
+                if self.causal:
+                    indices = range(i-(self.num_weight-1),i+1)
+                else:
+                    half = self.num_weight/2 #type is int, so no decimals
+                    indices = range(i-(self.num_weight-1-half),i+1+half)
+                if self.bias:
+                    x_tap = np.concatenate([np.array([1]),cgm[indices]])
+                else:
+                    x_tap = cgm[indices]
+                error = bgm[i-self.delay] - np.dot(x_tap, W)
+                W = W + 2*self.mu*error*x_tap
+                weight_path.append(W)
+                error_path.append(error)
+        for i in range(   ((end_idx - start_idx)/2), end_idx+1):
+            if self.causal:
+                indices = range(i-(self.num_weight-1),i+1)
+            else:
+                half = self.num_weight/2 #type is int, so no decimals
+                indices = range(i-(self.num_weight-1-half),i+1+half)
+            if self.bias:
+                x_tap = np.concatenate([np.array([1]),cgm[indices]])
+            else:
+                x_tap = cgm[indices]
+            if i > start_idx:
+                predicted[i] = np.dot(x_tap, W)
+            error = bgm[i-self.delay] - np.dot(x_tap, W)
+            weight_path.append(W)
+            error_path.append(error)
+
+        return (weight_path, predicted, error_path)
+    
+    def train(self, num_repeat = 10):
+        for k in range(num_repeat):
+            for key, value in self.training_set.iteritems():
+                W = self.weight # initalize weights
+                #value = np.array(value)
+                cgm = value[:, 1]
+                bgm = value[:, 2]
+                
+                # set start and end index for training, taking into account delay and causality
+                start_idx = np.max([self.num_weight-1, self.delay])
+                if self.causal:
+                    end_idx = len(cgm)-1
+                    start_idx = np.max([self.num_weight-1, self.delay])
+                else:
+                    end_idx = len(cgm)-1-(self.num_weight/2)
+                    start_idx = np.max([self.num_weight/2, self.delay])
+                    
+                #end_idx = len(cgm)-1 if self.causal else len(cgm)-1-(self.num_weight/2)
+                for i in range(start_idx, end_idx+1):
+                    if self.causal:
+                        indices = range(i-(self.num_weight-1),i+1)
+                    else:
+                        half = self.num_weight/2 #type is int, so no decimals
+                        indices = range(i-(self.num_weight-1-half),i+1+half)
+                    if self.bias:
+                        x_tap = np.concatenate([np.array([1]),cgm[indices]])
+                    else:
+                        x_tap = cgm[indices]
+                    error = bgm[i-self.delay] - np.dot(x_tap,W)
+                    W = W + 2*self.mu*error*x_tap
+                self.weight = W # update weights after each patient
+    
+    # test signal should be a 1d array
+    def apply_filter(self, test_signal):
+        filtered_signal = np.zeros(len(test_signal))
+        filtered_signal.fill(np.nan)
+        
+        # set end idex for filtering, taking into causality
+        if self.causal:
+            end_idx = len(test_signal)-1
+        else:
+            end_idx = len(test_signal)-1-(self.num_weight/2)
+        #end_idx = len(cgm)-1 if self.causal else len(cgm)-1-(self.num_weight/2)
+        
+        for i in range(self.num_weight-1, end_idx+1):
+            if self.causal:
+                indices = range(i-(self.num_weight-1),i+1)
+            else:
+                half = self.num_weight/2 #type is int, so no decimals
+                indices = range(i-(self.num_weight-1-half),i+1+half)
+            if self.bias:
+                x_tap = np.concatenate([np.array([1]),test_signal[indices]])
+            else:
+                x_tap = test_signal[indices]
+            filtered_signal[i] = np.dot(x_tap, self.weight)
+
+        # apply delay
+        filler = np.zeros(self.delay)
+        filler.fill(np.nan)
+        filtered_signal = np.concatenate([filtered_signal[self.delay:], filler])
+
+        return filtered_signal
+    
+    def est_trace_R(self):
+        s_sq_avg = np.zeros((self.num_weight))
+        for key, value in self.training_set.iteritems():
+            y = value[:, 1]
+            s_sq = np.zeros((self.num_weight))
+            for i in range(len(y)-self.num_weight+1):
+                x = y[i:i+self.num_weight]
+                s_sq = s_sq + x*x
+            s_sq = s_sq/i
+            s_sq_avg = s_sq_avg + s_sq
+        s_sq_avg = s_sq_avg/len(self.training_set)
+        return sum(s_sq_avg)
+
+# This class implements alpha LMS
+# Update equation found in Handout 9 in class notes
+class alpha_LMS_filter:
+    
+    def __init__(self, training_set, alpha=0.01, num_weight=5, bias=False, causal=True, delay=0):
+        self.training_set = training_set
+        self.num_weight = num_weight
+        self.alpha = alpha
+        self.bias = bias
+        self.causal = causal
+        self.delay = delay
+        if bias:
+            self.weight = np.zeros((self.num_weight+1))
+        else:
+            self.weight = np.zeros((self.num_weight))
+    
+    def reset_weight(self):
+        self.weight = np.zeros(len(self.weight))
+       
+    def train_single_patient(self, patient_id, stop_half = False, train_cycles=5):
+        W = self.weight # initalize weights
+        weight_path = []
+        error_path = []
+        value = self.training_set[patient_id]
+        cgm = value[:, 1]
+        bgm = value[:, 2]
+        
+        # set start and end index for training, taking into account delay and causality
+        start_idx = np.max([self.num_weight-1, self.delay])
+        if self.causal:
+            end_idx = len(cgm)-1
+            start_idx = np.max([self.num_weight-1, self.delay])
+        else:
+            end_idx = len(cgm)-1-(self.num_weight/2)
+            start_idx = np.max([self.num_weight/2, self.delay])
+        predicted = np.zeros(len(cgm))
+        predicted.fill(np.nan)
+        #end_idx = len(cgm)-1 if self.causal else len(cgm)-1-(self.num_weight/2)
+        for cycle in range(train_cycles):
+            for i in range(start_idx, (end_idx - start_idx)/2):
+                if self.causal:
+                    indices = range(i-(self.num_weight-1),i+1)
+                else:
+                    half = self.num_weight/2 #type is int, so no decimals
+                    indices = range(i-(self.num_weight-1-half),i+1+half)
+                if self.bias:
+                    x_tap = np.concatenate([np.array([1]),cgm[indices]])
+                else:
+                    x_tap = cgm[indices]
+                error = bgm[i-self.delay] - np.dot(x_tap, W)
+                W = W + self.alpha*error/(np.linalg.norm(x_tap)**2)*x_tap
+                weight_path.append(W)
+                error_path.append(error)
+        for i in range(   ((end_idx - start_idx)/2), end_idx+1):
+            if self.causal:
+                indices = range(i-(self.num_weight-1),i+1)
+            else:
+                half = self.num_weight/2 #type is int, so no decimals
+                indices = range(i-(self.num_weight-1-half),i+1+half)
+            if self.bias:
+                x_tap = np.concatenate([np.array([1]),cgm[indices]])
+            else:
+                x_tap = cgm[indices]
+            if i > start_idx:
+                predicted[i] = np.dot(x_tap, W)
+            error = bgm[i-self.delay] - np.dot(x_tap, W)
+            weight_path.append(W)
+            error_path.append(error)
+
+        return (weight_path, predicted, error_path)
+    def train(self, num_repeat = 10):
+        for k in range(num_repeat):
+            for key, value in self.training_set.iteritems():
+                W = self.weight # initalize weights
+                #value = np.array(value)
+                cgm = value[:, 1]
+                bgm = value[:, 2]
+                
+                # set start and end index for training, taking into account delay and causality
+                start_idx = np.max([self.num_weight-1, self.delay])
+                if self.causal:
+                    end_idx = len(cgm)-1
+                    start_idx = np.max([self.num_weight-1, self.delay])
+                else:
+                    end_idx = len(cgm)-1-(self.num_weight/2)
+                    start_idx = np.max([self.num_weight/2, self.delay])
+                    
+                #end_idx = len(cgm)-1 if self.causal else len(cgm)-1-(self.num_weight/2)
+                for i in range(start_idx, end_idx+1):
+                    if self.causal:
+                        indices = range(i-(self.num_weight-1),i+1)
+                    else:
+                        half = self.num_weight/2 #type is int, so no decimals
+                        indices = range(i-(self.num_weight-1-half),i+1+half)
+                    if self.bias:
+                        x_tap = np.concatenate([np.array([1]),cgm[indices]])
+                    else:
+                        x_tap = cgm[indices]
+                    error = bgm[i-self.delay] - np.dot(x_tap,W)
+                    W = W + self.alpha*error/(np.linalg.norm(x_tap)**2)*x_tap
+                self.weight = W # update weights after each patient
+    
+    # test signal should be a 1d array
+    def apply_filter(self, test_signal):
+        filtered_signal = np.zeros(len(test_signal))
+        filtered_signal.fill(np.nan)
+        
+        # set end idex for filtering, taking into causality
+        if self.causal:
+            end_idx = len(test_signal)-1
+        else:
+            end_idx = len(test_signal)-1-(self.num_weight/2)
+        #end_idx = len(cgm)-1 if self.causal else len(cgm)-1-(self.num_weight/2)
+        
+        for i in range(self.num_weight-1, end_idx+1):
+            if self.causal:
+                indices = range(i-(self.num_weight-1),i+1)
+            else:
+                half = self.num_weight/2 #type is int, so no decimals
+                indices = range(i-(self.num_weight-1-half),i+1+half)
+            if self.bias:
+                x_tap = np.concatenate([np.array([1]),test_signal[indices]])
+            else:
+                x_tap = test_signal[indices]
+            filtered_signal[i] = np.dot(x_tap, self.weight)
+
+        # apply delay
+        filler = np.zeros(self.delay)
+        filler.fill(np.nan)
+        filtered_signal = np.concatenate([filtered_signal[self.delay:], filler])
+        
+        return filtered_signal
---
+++ scripts/readEDF.py
@@ -0,0 +1,108 @@
+import numpy as np
+import os
+import pandas as pd
+import pyedflib
+import re
+
+def extract_summary_data(f, temp_d):
+	summary = f.read()
+	summary = summary.split('\n\n')
+	temp_d['freq'] = float(summary[0].split(' ')[3])
+	for line in summary:
+		if 'File Name:' in line:
+			temp = filter(None, line.split('\n'))
+			key = temp[0].split(' ')[2]
+			if len(temp) == 4:
+				temp_d[key] = []
+				continue
+			temp = temp[4:]
+			seizure_time = []
+			for x,y in zip(temp[0::2], temp[1::2]):
+				x = int(x.split(' ')[-2])
+				y = int(y.split(' ')[-2])
+				seizure_time.append((x,y))
+			temp_d[key] = seizure_time
+
+def read_edf(filename):
+	f = pyedflib.EdfReader(os.path.join(root, filename))
+	assert(np.all(f.getSampleFrequencies() == f.getSampleFrequencies()[0]))
+	n = f.signals_in_file
+	signal_labels = f.getSignalLabels()
+	sigbufs = np.zeros((n, f.getNSamples()[0]))
+	for i in np.arange(n):
+		sigbufs[i, :] = f.readSignal(i)
+	return sigbufs, f.getSignalLabels()
+	
+def label_edf(sigbufs, seizure_time, eeg_label):
+	time = np.arange(sigbufs.shape[1])/temp_d['freq']
+	seizure_id = np.zeros(sigbufs.shape[1])
+	seizure_delay = np.zeros(sigbufs.shape[1])
+	if seizure_time:
+		prev_end = 0
+		for x,y in seizure_time:
+			start_idx = int(temp_d['freq']*x)
+			end_idx = int(temp_d['freq']*y)+1 # Include the end point within the data
+			seizure_id[start_idx:end_idx].fill(1)
+			seizure_delay[prev_end:start_idx] = np.arange(start_idx-prev_end)[::-1]/temp_d['freq']
+			seizure_delay[end_idx:].fill(np.inf)
+			prev_end = end_idx
+	else:
+		seizure_delay += np.inf
+	df = np.vstack([time, seizure_id, seizure_delay, sigbufs])
+	df = df.T
+	df = df.astype('float32')
+	eeg_label = ['time', 'seizure', 'seizure_delay'] + eeg_label
+	df = pd.DataFrame(df, columns=eeg_label)
+	df['seizure'] = df['seizure'].astype('int8')
+	return df
+
+# Use the function like this
+# df = read_single_edf('chb10_89.edf')
+def read_single_edf(filename):
+	""" Reads a single edf file into a labeld data frame 
+
+	Use the function like
+	> import readEDF 
+	> df = read_single_edf('chb10_89.edf'))
+	"""
+	if '/' not in filename:
+		data_folder = '/'.join(os.getcwd().split('/')[:-1]) + '/ANES212_data/'
+		patient_folder = filename.split('_')[0]
+		filename = data_folder + patient_folder + '/' + filename
+	else:
+		data_folder = '/'.join(os.getcwd().split('/')[:-1]) + '/ANES212_data/'
+		patient_folder = filename.split('/')[-2]
+	
+	# Extract label data for the filename (e.g. start and end of seizures if any)
+	summary_file = data_folder + patient_folder + '/' + patient_folder + '-summary.txt'
+	d_temp = []
+	with open(summary_file, 'rb') as f:
+		extract_summary_data(f, temp_d)
+	
+	# Now read edf and label
+	sigbufs, eeg_label = read_edf(full_path)
+	df = label_edf(sigbufs, temp_d[filename.split('/')[-1]], eeg_label)
+	return df
+
+def read_patient_edf(patient_list, save = False):
+	data_folder = '/'.join(os.getcwd().split('/')[:-1]) + '/ANES212_data/'
+	patient_dict = {}
+	df_dict = {}
+	for root, dirs, files in os.walk(data_folder):
+		df_list = []
+		if any([patient_folder in root for patient_folder in patient_folder_list]):
+			temp_d = {} # Stores data from the summary txt file
+			for filename in files:
+				full_path = os.path.join(root, filename)
+				print filename
+				if '.edf'in filename and '.seizures' not in filename:
+					sigbufs, eeg_label = read_edf(full_path)
+					df = label_edf(sigbufs, temp_d[filename], eeg_label)
+					df_filename = os.path.join(root, filename.split('.')[0]) + '.csv'
+					if save:
+						df.to_csv(df_filename, sep='\t')
+				elif '-summary.txt' in filename:
+					with open(full_path, 'rb') as f:
+						extract_summary_data(f, temp_d)
+			df_dict[filename.split('_')[0]] = df_list
+	return df_dict
